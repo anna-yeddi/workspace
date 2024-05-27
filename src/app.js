2b8ed955fdc8058f8c31f8803b06fc30d26a2c4f
Anna Kova <m3ta007@gmail.com>
2024-05-26 22:33:18 -0400

    Added ESLint

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 const characters = [
-	{ name: 'swordbearer', health: 10 },
-	{ name: 'wizard', health: 100 },
-	{ name: 'wizard', health: 0 },
-	{ name: 'archer', health: 0 },
-]
+  { name: 'swordbearer', health: 10 },
+  { name: 'wizard', health: 100 },
+  { name: 'wizard', health: 0 },
+  { name: 'archer', health: 0 },
+];
 
-const alive = characters.filter((item) => item.health > 0)
+const alive = characters.filter((item) => item.health > 0);
+console.log(alive);
